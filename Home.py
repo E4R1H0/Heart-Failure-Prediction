325f3dbfc17194e3f75e5de979f74fcaaee1b156
EEEE <119985774+E4R1H0@users.noreply.github.com>
2024-01-26 20:37:17 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier 
 
 
 html_1 = """
