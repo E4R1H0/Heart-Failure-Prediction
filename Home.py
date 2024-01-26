a58120efb63231b5b9bb5e2dc42668f0329c6220
EEEE <119985774+E4R1H0@users.noreply.github.com>
2024-01-26 16:23:41 +0700

    .

--- Home.py
+++ Home.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from keras.layers import Dense, BatchNormalization, Dropout, LSTM
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras import callbacks
+from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
+
 
 
 col1, col2, col3 = st.columns([1.5, 6, 1])
