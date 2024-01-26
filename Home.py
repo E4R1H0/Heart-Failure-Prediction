943a1e7d17eddb94a51003f85a65237979d6ad01
EEEE <119985774+E4R1H0@users.noreply.github.com>
2024-01-26 20:28:18 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,17 +2,16 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
-from keras.layers import Dense, BatchNormalization, Dropout, LSTM
-from keras.models import Sequential
-from keras.utils import to_categorical
-from keras import callbacks
-from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
+from sklearn.naive_bayes import GaussianNB
 
 
+html_1 = """
+<div style="background-color:#0E1117;margin-top:40px;padding:5px;border-radius:5px;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h4>การทำนายอัตราเสียชีวิตจากภาวะหัวใจล้มเหลวของผู้ป่วยโรคหัวใจและหลอดเลือดที่อายุมากกว่า 50 ปี</h4><h5>Predicting the Risk of Death from Heart Failure in Patients with Cardiovascular Disease over 50 years old</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
 
 col1, col2, col3 = st.columns([1.5, 6, 1])
 
@@ -20,37 +19,269 @@ with col1:
     st.write("") 
 
 with col2:
-    st.image("./pic/dataset-cover.jpg")
+    st.image("./pic/home.png")
 
 with col3:
     st.write("")
 
+### Visualization ###
 
-html_1 = """
-<div style="background-color:#0E1117;margin-top:40px;padding:5px;border-radius:5px;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
-<center><h4 style ="filter: drop-shadow(0px 0px 5px #89c5fb);">ระบบวิเคราะห์สุขภาพการนอนหลับและไลฟ์สไตล์โดยใช้เทคนิคเหมืองข้อมูล</h4><h5  style ="filter: drop-shadow(0px 0px 5px #89c5fb);">The system analyzes health, sleep and lifestyle using data mining techniques</h5></center>
-</div>
-"""
-st.markdown(html_1, unsafe_allow_html=True)
-st.markdown("")
-
-html_2 = """
-<div style="background-color:#0E1117;padding:15px;">
-<center></center><left><h6 style="text-indent: 30px;line-height: 1.5;">การรักษาสุขภาพการนอนมีความสำคัญสำหรับความเป็นอยู่ของบุคคลทั้งทางร่างกายและจิตใจทุกวัย การนอนไม่เพียงเติมพลังให้ร่างกายและสมองเท่านั้น แต่ยังมีผลที่สำคัญต่อระบบฮอร์โมน การฟื้นตัวของร่างกาย และส่งเสริมสุขภาพจิตใจด้วย การนอนไม่เพียงพอสามารถทำให้ความต้านทานลดลง, มีปัญหาการคิด, และการเรียนรู้มีผลกระทบ นอนน้อยยังเสี่ยงต่อโรคต่างๆ เช่น โรคอ้วน, โรคหัวใจ, วิตกกังวล, หรือโรคจิตเร้าอารมณ์, และภาวะซึมเศร้า การวิเคราะห์ข้อมูลการนอนเป็นทางการเพื่อให้คนทั่วไปทราบถึงปัญหาของตนเอง การใช้ 3 เทคนิคในการวิเคราะห์ได้แก่ เพื่อนบ้านใกล้สุด (K-nearest Neighbors), ต้นไม้ตัดสินใจ (Decision Tree), และ นาอีฟเบย์ (Naive Bayes) เป็นเครื่องมือที่มีประสิทธิภาพในการนำข้อมูลมาวิเคราะห์ ผลการวิเคราะห์นี้จะช่วยให้บุคคลที่สนใจสามารถเข้าใจถึงสถานะของการนอนของตนเอง และสามารถตัดสินใจดำเนินการรักษาหรือปรับเปลี่ยนพฤติกรรมการนอนในทางที่เหมาะสม เพื่อรักษาสุขภาพทั้งร่างกายและจิตใจให้มีคุณภาพดีที่สุด ผลการวิเคราะห์พบว่าเทคนิค Decision Tree มีความแม่นยำสูงที่สุดที่ระดับ 0.89, ตามด้วย Naive Bayes ที่มีความแม่นยำที่ 0.88 และ K-nearest Neighbors ที่มีความแม่นยำที่ 0.87 จากผลลัพธ์นี้, ผู้วิจัยจึงเลือกใช้ Decision Tree เป็นเทคนิคในการพัฒนาระบบทำนายสถานะการนอนของผู้ใช้งานในเว็บไซต์ ทั้งนี้, ผู้ใช้งานสามารถทดสอบระบบเพื่อตรวจสอบสถานะการนอนของตนเองได้</h6></left>
-<center></center><left><h6 style="text-indent: 30px;line-height: 1.5;">Maintaining good sleep health is crucial for overall well-being, both physically and mentally, at all ages. Sleep not only replenishes energy for the body and brain but also plays a significant role in hormone regulation, body recovery, and mental health promotion. Insufficient sleep can lead to weakened immunity, cognitive problems, and impaired learning. Additionally, inadequate sleep is associated with various health risks, such as obesity, heart disease, anxiety disorders, depression, and even conditions like sleep disorders.
-
-Analyzing sleep data formally can provide valuable insights into an individual's sleep issues. Using three effective techniques—K-nearest Neighbors, Decision Tree, and Naive Bayes—facilitates the analysis process. These tools efficiently process data to help individuals understand their sleep patterns better. Sleep deprivation can result in reduced resistance, cognitive difficulties, and increased susceptibility to various diseases.
-
-The results of the analysis indicate that Decision Tree has the highest accuracy level at 0.89, followed by Naive Bayes at 0.88 and K-nearest Neighbors at 0.87. Based on these findings, researchers have chosen to implement the Decision Tree technique to develop a predictive system for users' sleep status on a website. Users can utilize this system to assess their own sleep status and make informed decisions to improve their sleep habits, ultimately enhancing both physical and mental well-being.</h6></left>
-</div>
-"""
-st.markdown(html_2, unsafe_allow_html=True)
-st.markdown("")
+df = pd.read_excel('./data/heart.xlsx')
 
 html_3 = """
-<div style="background-color:#0E1117;border-top: 3px solid #ffffff;">
-<center><h4 style="text-indent: 50%;">Wiriya Hemmala 64/44 007</h4></center>
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>Example data table</h3></center>
 </div>
 """
 st.markdown(html_3, unsafe_allow_html=True)
 st.markdown("")
+st.write(df.head(10))
+
+html_4 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>Count plot for various categorical features</h3></center>
+</div>
+"""
+st.markdown(html_4, unsafe_allow_html=True)
+st.markdown("")
+
+# fig = plt.figure(figsize=(18, 15))
+# gs = fig.add_gridspec(3, 3)
+# gs.update(wspace=0.5, hspace=0.25)
+# ax0 = fig.add_subplot(gs[0, 0])
+# ax1 = fig.add_subplot(gs[0, 1])
+# ax2 = fig.add_subplot(gs[0, 2])
+# ax3 = fig.add_subplot(gs[1, 0])
+# ax4 = fig.add_subplot(gs[1, 1])
+# ax5 = fig.add_subplot(gs[1, 2])
+# ax6 = fig.add_subplot(gs[2, 0])
+# ax7 = fig.add_subplot(gs[2, 1])
+# ax8 = fig.add_subplot(gs[2, 2])
+
+# background_color = "#ffe6e6"
+# color_palette = ["#800000", "#8000ff", "#6aac90", "#5833ff", "#da8829"]
+# fig.patch.set_facecolor(background_color)
+# ax0.set_facecolor(background_color)
+# ax1.set_facecolor(background_color)
+# ax2.set_facecolor(background_color)
+# ax3.set_facecolor(background_color)
+# ax4.set_facecolor(background_color)
+# ax5.set_facecolor(background_color)
+# ax6.set_facecolor(background_color)
+# ax7.set_facecolor(background_color)
+# ax8.set_facecolor(background_color)
+
+# # Title of the plot
+# ax0.spines["bottom"].set_visible(False)
+# ax0.spines["left"].set_visible(False)
+# ax0.spines["top"].set_visible(False)
+# ax0.spines["right"].set_visible(False)
+# ax0.tick_params(left=False, bottom=False)
+# ax0.set_xticklabels([])
+# ax0.set_yticklabels([])
+# ax0.text(0.5, 0.5,
+#          'Count plot for various\n categorical features\n_________________',
+#          horizontalalignment='center',
+#          verticalalignment='center',
+#          fontsize=18, fontweight='bold',
+#          fontfamily='serif',
+#          color="#000000")
+
+# # Function to create count plot using bar
+# def create_count_plot(ax, data, x, palette):
+#     ax.bar(data[x].value_counts().index, data[x].value_counts().values, color=palette)
+#     ax.grid(color='#000000', linestyle=':', axis='y', zorder=0, dashes=(1, 5))
+#     ax.set_xlabel("")
+#     ax.set_ylabel("")
+
+# # Sex count
+# ax1.text(0.3, 165, 'Sex', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax1, df, 'sex', color_palette)
+# ax1.set_xticks([0, 1])
+# ax1.set_xticklabels(["Female(0)", "Male(1)"])
+
+# # Exng count
+# ax2.text(0.3, 160, 'Exng', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax2, df, 'exng', color_palette)
+# ax2.set_xticks([0, 1])
+# ax2.set_xticklabels(["No(0)","Yes(1)"])
+
+# # Caa count
+# ax3.text(1.5, 120, 'Caa', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax3, df, 'caa', color_palette)
+# ax3.set_xticks([0, 1,2,3,4])
+
+
+# # Cp count
+# ax4.text(1.5, 120, 'Cp', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax4, df, 'cp', color_palette)
+# ax4.set_xticks([0,1,2,3])
+# ax4.set_xticklabels(["Typical angina(1)","Atypical angina(2)","nopain(3)","asymptomatic(4)"], rotation=15)
+
+# # Fbs count
+# ax5.text(0.5, 200, 'Fbs', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax5, df, 'fbs', color_palette)
+# ax5.set_xticks([0, 1])
+# ax5.set_xticklabels(["False(0)","True(1)"])
+
+# # Restecg count
+# ax6.text(0.75, 120, 'Restecg', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax6, df, 'restecg', color_palette)
+# ax6.set_xticks([0, 1,2])
+# ax6.set_xticklabels(["normal(0)","ST-T abnormality (1)","LV hypertrophy(2)"], rotation=15)
+
+# # Slp count
+# ax7.text(0.85, 120, 'Slp', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax7, df, 'slp', color_palette)
+# ax7.set_xticks([0, 1,2])
+
+# # Thall count
+# ax8.text(1.2, 120, 'Thall', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# create_count_plot(ax8, df, 'thall', color_palette)
+# ax8.set_xticks([0, 1, 2, 3])
+
+# # Remove spines
+# for s in ["top", "right", "left"]:
+#     ax1.spines[s].set_visible(False)
+#     ax2.spines[s].set_visible(False)
+#     ax3.spines[s].set_visible(False)
+#     ax4.spines[s].set_visible(False)
+#     ax5.spines[s].set_visible(False)
+#     ax6.spines[s].set_visible(False)
+#     ax7.spines[s].set_visible(False)
+#     ax8.spines[s].set_visible(False)
+
+
+# st.pyplot(fig)
+
+# html_5 = """
+# <div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+# <center><h3>Count of the target</h3></center>
+# </div>
+# """
+# st.markdown(html_5, unsafe_allow_html=True)
+# st.markdown("")
+
+
+
+# fig = plt.figure(figsize=(18, 7))
+# gs = fig.add_gridspec(1, 2)
+# gs.update(wspace=0.3, hspace=0.15)
+# ax0 = fig.add_subplot(gs[0, 0])
+# ax1 = fig.add_subplot(gs[0, 1])
+
+# background_color = "#ffe6e6"
+# color_palette = ["#800000", "#8000ff", "#6aac90", "#5833ff", "#da8829"]
+# fig.patch.set_facecolor(background_color)
+# ax0.set_facecolor(background_color)
+# ax1.set_facecolor(background_color)
+
+# # Title of the plot
+# ax0.text(0.5, 0.5, "Count of the target\n___________",
+#          horizontalalignment='center',
+#          verticalalignment='center',
+#          fontsize=18,
+#          fontweight='bold',
+#          fontfamily='serif',
+#          color='#000000')
+
+# ax0.set_xticklabels([])
+# ax0.set_yticklabels([])
+# ax0.tick_params(left=False, bottom=False)
+
+# # Target Count
+# ax1.text(0.35, 130, "Output", fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+# ax1.grid(color='#000000', linestyle=':', axis='y', zorder=0, dashes=(1, 5))
+# ax1.bar(df['output'].value_counts().index, df['output'].value_counts().values, color=color_palette)
+# ax1.set_xlabel("")
+# ax1.set_ylabel("")
+# ax1.set_xticks([0, 1])
+# ax1.set_xticklabels(["Low chances of attack(0)", "High chances of attack(1)"])
+
+# # Remove spines
+# for s in ["top", "left", "right"]:
+#     ax0.spines[s].set_visible(False)
+#     ax1.spines[s].set_visible(False)
+# st.pyplot(fig)
+
+
+### Analysis ###
+
+html_6 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
+<center><h3>Example data table</h3></center>
+</div>
+"""
+st.markdown(html_6, unsafe_allow_html=True)
+st.markdown("")
+st.write(df.head(10))
+
+html_7 = """
+<div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;margin-top:20px;">
+<center><h3>Prediction</h3></center>
+</div>
+"""
+st.markdown(html_7, unsafe_allow_html=True)
+st.markdown("")
+
+
+s1 = st.number_input("อายุของผู้ป่วย")
+s2 = st.selectbox("โรคโรหิตจาง (0 : ไม่เป็น | 1 : เป็น)",0,1)
+s3 = st.number_input("ระดับเอนไซม์CPKในเลือด(mcg/L)")
+s4 = st.selectbox("โรคเบาหวาน (0 : ไม่เป็น | 1 : เป็น)",0,1)
+s5 = st.number_input("อัตราส่วนร้อยละของเลือดที่สูบออกจากหัวใจในแต่ละครั้ง(ร้อยละ)")
+s6 = st.selectbox("โรคความดันโลหิตสูง (0 : ไม่เป็น | 1 : เป็น)",0,1)
+s7 = st.number_input("จำนวนเกล็ดเลือดในเลือด (พันเกล็ดเลือด/มิลลิลิตร)")
+s8 = st.number_input("ปริมาณครีเอทินีนในซีรั่ม (มิลลิกรัม/เดซิลลิตร)")
+s9 = st.number_input("ปริมาณโซเดียมในซีรั่ม (มิลลิเอควิเทลนต์/ลิตร)")
+s10 = st.selectbox("เพศ (0 : ผู้หญิง | 1 : ผู้ชาย)",0,1)
+s11 = st.selectbox("การสูบบุหรี่ (0 : ไม่ได้สูบ | 1 : สูบ)",0,1)
+s12 = st.selectbox("ระยเวลาในการติดตามผู้ป่วย")
+
+if st.button("ทำนายผล"):
+
+   X=df.drop(["DEATH_EVENT"],axis=1)
+   y=df["DEATH_EVENT"]
+
+   ds_model = DecisionTreeClassifier()
+   ds_model.fit(X, y)
+
+
+   x_input = np.array([[s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12]])
+
+   out = ds_model.predict(x_input)
+
+   if out[0]== 0 :
+          
+      html_8 = """
+      <div style="background-color:#0E1117;padding:20px;border: 3px solid #ffffff;">
+      <center><h3 style="border-bottom: 3px solid #ffffff;">ไม่มีความเสี่ยงในการเสียชีวิต</h3></center>
+      </div>
+      """
+      st.markdown(html_8, unsafe_allow_html=True)
+      st.markdown("")
+
+   elif out[0]==1:
+          
+          
+      html_9 = """
+      <div style="background-color:#0E1117;padding:20px;border: 3px solid #ffffff;">
+      <center><h3 style="border-bottom: 3px solid #ffffff;">โอกาสหัวใจวายมาก</h3></center>
+      <left><h6 style="text-indent: 30px;line-height: 1.5;padding-top:15px;">โรคหัวใจวาย (Coronary Artery Disease, CAD) เป็นภาวะที่เกิดจากการสะสมของเส้นเลือดในหัวใจ (หลอดเลือดในหัวใจ) ทำให้การไหลของเลือดที่จำเป็นสำหรับการให้สารอาหารและออกซิเจนให้กับกล้ามเนื้อหัวใจลดลง สาเหตุหลักของโรคหัวใจวายคือการสะสมของตะกอนไขมันและแร่ธาตุในเส้นเลือดในหัวใจ (เช่น เนื้อเยื่อไขมัน, เซลล์เลือดขาว, และแคลเซียม) ซึ่งทำให้เกิดลิ่ม (plaque) ในเส้นเลือด ลิ่มนี้จะทำให้เส้นเลือดตีบ และสามารถทำให้เกิดอาการหัวใจวาย นี่คือบางวิธีที่อาจช่วยลดโอกาสหัวใจวาย</h6></left>
+      <ul>
+         <li>รับประทานอาหารที่สุขภาพดี:การบริโภคอาหารที่รวมถึงผัก, ผลไม้, และอาหารที่มีไขมันไม่เบาหรือไขมันดีสามารถช่วยลดความเสี่ยง</li>
+         <li>ออกกำลังกาย:การมีกิจกรรมทางกายเป็นประจำ อย่างน้อย 150 นาทีต่อสัปดาห์ เช่น เดินเร็ว, วิ่ง, หรือว่ายน้ำ</li>
+         <li>ควบคุมน้ำหนัก:การรักษาน้ำหนักที่เหมาะสมสามารถช่วยลดโอกาสหัวใจวาย</li>
+         <li>การเลิกสูบบุหรี่:การเลิกสูบบุหรี่จะช่วยลดความเสี่ยงของโรคหัวใจวาย</li>
+         <li>ควบคุมและรักษาโรคประจำตัว:การควบคุมโรคเบาหวาน, ความดันโลหิต, และโรคอื่น ๆ ที่เป็นปัจจัยเสี่ยง</li>
+         <li>การบริหารจัดการสเตรส:การใช้เทคนิคการบริหารจัดการสเตรส เช่น การฝึกสมาธิหรือการทำโยคะ</li>
+         <li>การดื่มแอลกอฮอล์อย่างมีสติ:การดื่มแอลกอฮอล์ในปริมาณที่ปลอดภัยมีผลต่อการลดความเสี่ยง</li>
+         <li>การตรวจสุขภาพประจำปี:การตรวจสุขภาพประจำปีเพื่อตรวจสอบสุขภาพโดยรวมและความเสี่ยงต่อโรค</li>
+         <li>การนอนหลับเพียงพอ:การนอนหลับประจำวันในปริมาณที่เหมาะสมสามารถส่งเสริมสุขภาพหัวใจ</li>
+         <li>การหลีกเลี่ยงการบริโภคอาหารที่มีสารอาหารไม่ดี:ลดการบริโภคอาหารที่มีโคเลสเตอรอลสูง, น้ำตาล, และเกลือ</li>
+      </ul>
+      <left><h6 style="text-indent: 30px;line-height: 1.5;padding-top:15px;">การปฏิบัติตนตามแนวทางดังกล่าวจะช่วยลดโอกาสที่จะเป็นโรคหัวใจวายและส่งเสริมสุขภาพหัวใจที่แข็งแรง. อย่าลืมปรึกษาแพทย์หากคุณมีปัญหาสุขภาพหรือต้องการคำแนะนำเพิ่มเติมเกี่ยวกับการรักษาและป้องกันโรคหัวใจวาย</h6></left>
+      </div>
+      """
+      st.markdown(html_9, unsafe_allow_html=True)
+      st.markdown("")
